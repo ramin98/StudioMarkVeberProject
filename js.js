ba72e4098e2831dbfe681546e045fd30a23cb1da
ramin98 <59912261+ramin98@users.noreply.github.com>
2021-07-20 16:01:07 +0400

    Add files via upload

---
+++ js.js
@@ -0,0 +1,71 @@
+$( ".place-one" ).mouseover(function(){ 
+    $( ".places-div-subscribe" ).css({"display": "flex"});   
+}).mouseout(function(){ 
+    $( ".places-div-subscribe" ).css({"display": "none"});     
+});
+
+$( ".place-two" ).mouseover(function(){ 
+    $( ".places-div-subscribe" ).css({"display": "flex"}); 
+}).mouseout(function(){ 
+    $( ".places-div-subscribe" ).css( {"display": "none"}); 
+});
+
+$( ".place-three" ).mouseover(function(){ 
+    $( ".places-div-subscribe" ).css({"display": "flex"}); 
+}).mouseout(function(){ 
+    $( ".places-div-subscribe" ).css( {"display": "none"}); 
+});
+
+$( ".place-four" ).mouseover(function(){ 
+    $( ".places-div-subscribe" ).css({"display": "flex"}); 
+}).mouseout(function(){ 
+    $( ".places-div-subscribe" ).css( {"display": "none"}); 
+});
+
+$( ".place-five" ).mouseover(function(){ 
+    $( ".places-div-subscribe" ).css({"display": "flex"}); 
+}).mouseout(function(){ 
+    $( ".places-div-subscribe" ).css( {"display": "none"}); 
+});
+
+$( ".place-six" ).mouseover(function(){ 
+    $( ".places-div-subscribe" ).css({"display": "flex"}); 
+}).mouseout(function(){ 
+    $( ".places-div-subscribe" ).css( {"display": "none"}); 
+});
+
+$( ".place-seven" ).mouseover(function(){ 
+    $( ".places-div-subscribe" ).css({"display": "flex"}); 
+}).mouseout(function(){ 
+    $( ".places-div-subscribe" ).css( {"display": "none"}); 
+});
+
+$( ".place-eight" ).mouseover(function(){ 
+    $( ".places-div-subscribe" ).css({"display": "flex"}); 
+}).mouseout(function(){ 
+    $( ".places-div-subscribe" ).css( {"display": "none"}); 
+});
+
+$( ".place-nine" ).mouseover(function(){ 
+    $( ".places-div-subscribe" ).css({"display": "flex"}); 
+}).mouseout(function(){ 
+    $( ".places-div-subscribe" ).css( {"display": "none"}); 
+});
+
+function myFunction() {
+    $('.menu-tel-container').css({"display": "flex"})
+  }
+
+function button(){
+    $('.menu-tel-container').css({"display": "none"})
+}
+
+function slider_left() {
+    $('#myVideo').css({"display": "flex"})
+    $('.video-two').css({"display": "none"})
+  }
+
+function slider_right(){
+    $('#myVideo').css({"display": "none"})
+    $('.video-two').css({"display": "flex"})
+}
